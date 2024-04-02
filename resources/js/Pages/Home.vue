f7cfeeada89e651f2996367ab6ccbabd59677879
DannyS95 <dannys20.95@hotmail.com>
2024-04-02 22:34:02 +0100

    finish off template and tailwind styles

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -2,36 +2,40 @@
 import { useForm } from '@inertiajs/vue3'
 
 const form = useForm({
-  name: null,
-  avatar: null,
+    oldData: null,
+    recentData: null,
 })
 
 function submit() {
-  form.post('/users')
+    form.post('/api/dataFilesDifference')
 }
 </script>
 
 <template>
-  <form @submit.prevent="submit">
-    <label for="oldData" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">oldData</label>
-    <input
-        id="oldData"
-        type="file" @input="form.avatar = $event.target.files[0]"
-        class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-    />
-    <progress v-if="form.progress" :value="form.progress.percentage" max="100">
-      {{ form.progress.percentage }}%
-    </progress>
-
-    <label for="recentData" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">recentData</label>
-    <input
-        id="recentData" type="file"
-        @input="form.avatar = $event.target.files[0]"
-        class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-    />
-    <progress v-if="form.progress" :value="form.progress.percentage" max="100">
-      {{ form.progress.percentage }}%
-    </progress>
-    <button type="submit">Submit</button>
-  </form>
+    <div class="relative isolate px-6 pt-14 lg:px-8">
+        <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
+            <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
+        </div>
+        <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+        </div>
+        <div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]" aria-hidden="true">
+            <div class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
+        </div>
+    </div>
+    <form class="mx-auto max-w-4xl mt-10 flex items-center justify-center gap-x-6" @submit.prevent="submit">
+        <label for="oldData" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Old Data</label>
+        <input
+            id="oldData"
+            type="file" @input="form.oldData = $event.target.files[0]"
+            class="z-50 block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+        />
+        <label for="recentData" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Recent Data</label>
+        <input
+            id="recentData" type="file"
+            @input="form.recentData = $event.target.files[0]"
+            class="z-50 block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+        />
+        <button class="z-50 py-2.5 mt-2 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+            type="submit">Submit</button>
+    </form>
 </template>
